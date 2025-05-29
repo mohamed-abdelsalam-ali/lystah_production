8bc93a86272555f5bc8661041bf5ec938b70bb5b
unknown <prog.mohamed566@gmail.com>
2025-05-29 11:40:36 +0300

    1

--- config/services.php
+++ config/services.php
@@ -35,9 +35,7 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
-        'guzzle' => [
-        'verify' => false, // WARNING: Do not use this in production
-    ],
+      
     ],
 
 ];
